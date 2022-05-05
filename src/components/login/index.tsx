b793e6d8cfa64a44805b6a2c012958c0a7e5e4a4
seeodm <sdm0325@gmail.com>
2022-05-05 17:11:14 +0900

    feat: 초기 화면

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,5 +1,43 @@
+import styled from 'styled-components';
+
+import Logo from '#/asset/logo.svg';
+
 function Login() {
-  return <div>로그인하시죠!!</div>;
+  return (
+    <LoginContainer>
+      <TitleContainer>
+        <Title>내 냉장고... 어쩌지?</Title>
+        <img src={Logo} alt="logo" className="logo" />
+      </TitleContainer>
+    </LoginContainer>
+  );
 }
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 50px;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0 30px;
+
+  .logo {
+    height: 100%;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 1.6rem;
+
+  max-width: 50px;
+  word-break: keep-all;
+`;
 
 export default Login;
--- src/components/login/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const LoginContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  margin-top: 50px;
-`;
